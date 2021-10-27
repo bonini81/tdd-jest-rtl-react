05dc472433d432530fda1456a2038ecefa044b67
Dominguez Bonini Andrés Santiago <andres.dominguez@novaseguroslatam.com>
2021-10-27 08:21:06 -0500

    Test botones

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [buttonColor, setButtonColor]  = useState('red');
+  const [disabled, setDisabled] = useState(false);
+
+  const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
+  
   return (
-    <div className="App">
-     <h1>Learn React!</h1>
+    <div>
+     <button 
+     style={{ backgroundColor: buttonColor }}
+     onClick={( ()=> setButtonColor(newButtonColor) )}
+     disabled={disabled}
+     >Change to {newButtonColor}</button>
+     <br />
+     <input 
+     type="checkbox"
+     id="enable-button-checkbox"
+     defaultChecked={disabled}
+     aria-checked={disabled}
+     onChange={ (e) => setDisabled(e.target.checked)}
+     />
     </div>
   );
 }
